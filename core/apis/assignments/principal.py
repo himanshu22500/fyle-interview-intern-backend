f094f9862a308dd575ee6763aa50fccb78c41c48
himanshu22500 <himanshu22500@gmail.com>
2024-05-17 13:46:12 +0530

    Add helper to get DRAFT and SUBMITTED assignment

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -11,6 +11,22 @@ principal_assignments_resources = Blueprint('principal_assignments_resources', _
 @decorators.authenticate_principal
 def list_assignments(p):
     """Returns list of assignments"""
-    principals_assignments = Assignment.get_all_assignments()
+    principals_assignments = Assignment.get_all_submitted_and_graded_assignments()
     principals_assignments_dump = AssignmentSchema().dump(principals_assignments, many=True)
     return APIResponse.respond(data=principals_assignments_dump)
+
+@principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_assignment(p, incoming_payload):
+    """Grade an assignment"""
+    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    graded_assignment = Assignment.mark_grade(
+        _id=grade_assignment_payload.id,
+        grade=grade_assignment_payload.grade,
+        auth_principal=p
+    )
+    db.session.commit()
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(data=graded_assignment_dump)
