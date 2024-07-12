1137d89e6766b4e9712c4e11826bce78fc9fda23
himanshu22500 <himanshu22500@gmail.com>
2024-07-12 20:04:50 +0530

    Add test for teachers and bug fix in test

--- tests/principals_test.py
+++ tests/principals_test.py
@@ -1,3 +1,4 @@
+from core.libs.helpers import create_and_get_assignment_for_test
 from core.models.assignments import AssignmentStateEnum, GradeEnum
 
 
@@ -21,7 +22,7 @@ def test_grade_assignment_draft_assignment(client, h_principal):
     response = client.post(
         '/principal/assignments/grade',
         json={
-            'id': 5,
+            'id': create_and_get_assignment_for_test(state=AssignmentStateEnum.DRAFT),
             'grade': GradeEnum.A.value
         },
         headers=h_principal
@@ -60,3 +61,30 @@ def test_regrade_assignment(client, h_principal):
 
     assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
     assert response.json['data']['grade'] == GradeEnum.B
+
+
+def test_grade_assignment_unauthorized_access(client):
+    response = client.post(
+        '/principal/assignments/grade',
+        json={
+            'id': 1,
+            'grade': 'A'
+        }
+    )
+
+    # Check that the response has a status code of 401
+    assert response.status_code == 401
+
+def test_list_teachers(client, h_principal):
+    # Send a request to list teachers
+    response = client.get(
+        '/principal/teachers',
+        headers=h_principal
+    )
+
+    # Check that the response has a status code of 200
+    assert response.status_code == 200
+
+    # Check that the response contains a list of teachers
+    data = response.json
+    assert isinstance(data['data'], list)
